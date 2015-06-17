e6d8c67a76cf6cb1f7f16dfaff66859062a4543a
Seamus O Connor <seamo123@gmail.com>
2015-06-17 18:32:53 +0900

    Finished all tutorials

---
+++ src/Properties/Person.java
@@ -0,0 +1,29 @@
+package Properties;
+
+/**
+ * Created by Seamy on 6/15/2015.
+ */
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Person {
+
+    private StringProperty firstName = new SimpleStringProperty(this, "firstName", "");
+
+    //Returns the StringProperty object
+    public StringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    //Return the firstName value (ie. "Bucky")
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    //Set the firstName value
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+
+}
